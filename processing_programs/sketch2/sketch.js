6b57d1c965773ddee564a74096236d175c575381
dceccacci <71663434+dceccacci@users.noreply.github.com>
2020-10-24 19:07:40 -0700

    Update sketch.js
    added my sketch 2

--- processing_programs/sketch2/sketch.js
+++ processing_programs/sketch2/sketch.js
@@ -1,12 +1,1043 @@
 function setup() {
-    createCanvas(400,300);
-  }
-  
-  function draw() {
-    if(mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-      }
-    ellipse(mouseX,mouseY,80,80);
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+strokeWeight(0)
+background(220);
+   fill(0);
+   beginShape();
+   vertex(0,0);
+   vertex(50,-25);
+   vertex(50,0);
+   vertex(50,25);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(0,0);
+   vertex(50,25);
+   vertex(50,75);
+   vertex(0,50);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(0,50);
+   vertex(50,75);
+   vertex(0,100);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(0,100);
+   vertex(50,75);
+   vertex(50,125);
+   vertex(0,150);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(0,150);
+   vertex(50,125);
+   vertex(50,150);
+   vertex(50,175);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(0,150);
+   vertex(50,175);
+   vertex(50,225);
+   vertex(0,200);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(0,200);
+   vertex(50,225);
+   vertex(0,250);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(0,250);
+   vertex(50,225);
+   vertex(50,275);
+   vertex(0,300);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(0,300);
+   vertex(50,275);
+   vertex(50,300);
+   vertex(50,325);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(0,300);
+   vertex(50,325);
+   vertex(50,375);
+   vertex(0,350);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(0,350);
+   vertex(50,375);
+   vertex(0,400);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(0,400);
+   vertex(50,375);
+   vertex(50,425);
+   vertex(0,450);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(50,-25);
+   vertex(100,0);
+   vertex(50,25);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(50,25);
+   vertex(100,0);
+   vertex(100,50);
+   vertex(50,75);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(50,75);
+   vertex(100,50);
+   vertex(100,100);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(50,75);
+   vertex(100,100);
+   vertex(100,150);
+   vertex(50,125);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(50,125);
+   vertex(100,150);
+   vertex(50,175);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(50,175);
+   vertex(100,150);
+   vertex(100,200);
+   vertex(50,225);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(50,225);
+   vertex(100,200);
+   vertex(100,250);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(50,225);
+   vertex(100,250);
+   vertex(100,300);
+   vertex(50,275);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(50,275);
+   vertex(100,300);
+   vertex(50,325);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(50,325);
+   vertex(100,300);
+   vertex(100,350);
+   vertex(50,375);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(50,375);
+   vertex(100,350);
+   vertex(100,400);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(50,375);
+   vertex(100,400);
+   vertex(100,450);
+   vertex(50,425);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(100,0);
+   vertex(150,-25);
+   vertex(150,0);
+   vertex(150,25);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(100,0);
+   vertex(150,25);
+   vertex(150,75);
+   vertex(100,50);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(100,50);
+   vertex(150,75);
+   vertex(100,100);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(100,100);
+   vertex(150,75);
+   vertex(150,125);
+   vertex(100,150);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(100,150);
+   vertex(150,125);
+   vertex(150,150);
+   vertex(150,175);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(100,150);
+   vertex(150,175);
+   vertex(150,225);
+   vertex(100,200);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(100,200);
+   vertex(150,225);
+   vertex(100,250);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(100,250);
+   vertex(150,225);
+   vertex(150,275);
+   vertex(100,300);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(100,300);
+   vertex(150,275);
+   vertex(150,300);
+   vertex(150,325);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(100,300);
+   vertex(150,325);
+   vertex(150,375);
+   vertex(100,350);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(100,350);
+   vertex(150,375);
+   vertex(100,400);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(100,400);
+   vertex(150,375);
+   vertex(150,425);
+   vertex(100,450);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(150,-25);
+   vertex(200,0);
+   vertex(150,25);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(150,25);
+   vertex(200,0);
+   vertex(200,50);
+   vertex(150,75);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(150,75);
+   vertex(200,50);
+   vertex(200,100);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(150,75);
+   vertex(200,100);
+   vertex(200,150);
+   vertex(150,125);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(150,125);
+   vertex(200,150);
+   vertex(150,175);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(150,175);
+   vertex(200,150);
+   vertex(200,200);
+   vertex(150,225);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(150,225);
+   vertex(200,200);
+   vertex(200,250);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(150,225);
+   vertex(200,250);
+   vertex(200,300);
+   vertex(150,275);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(150,275);
+   vertex(200,300);
+   vertex(150,325);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(150,325);
+   vertex(200,300);
+   vertex(200,350);
+   vertex(150,375);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(150,375);
+   vertex(200,350);
+   vertex(200,400);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(150,375);
+   vertex(200,400);
+   vertex(200,450);
+   vertex(150,425);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(200,0);
+   vertex(250,-25);
+   vertex(250,0);
+   vertex(250,25);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(200,0);
+   vertex(250,25);
+   vertex(250,75);
+   vertex(200,50);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(200,50);
+   vertex(250,75);
+   vertex(200,100);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(200,100);
+   vertex(250,75);
+   vertex(250,125);
+   vertex(200,150);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(200,150);
+   vertex(250,125);
+   vertex(250,150);
+   vertex(250,175);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(200,150);
+   vertex(250,175);
+   vertex(250,225);
+   vertex(200,200);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(200,200);
+   vertex(250,225);
+   vertex(200,250);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(200,250);
+   vertex(250,225);
+   vertex(250,275);
+   vertex(200,300);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(200,300);
+   vertex(250,275);
+   vertex(250,300);
+   vertex(250,325);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(200,300);
+   vertex(250,325);
+   vertex(250,375);
+   vertex(200,350);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(200,350);
+   vertex(250,375);
+   vertex(200,400);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(200,400);
+   vertex(250,375);
+   vertex(250,425);
+   vertex(200,450);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(250,-25);
+   vertex(300,0);
+   vertex(250,25);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(250,25);
+   vertex(300,0);
+   vertex(300,50);
+   vertex(250,75);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(250,75);
+   vertex(300,50);
+   vertex(300,100);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(250,75);
+   vertex(300,100);
+   vertex(300,150);
+   vertex(250,125);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(250,125);
+   vertex(300,150);
+   vertex(250,175);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(250,175);
+   vertex(300,150);
+   vertex(300,200);
+   vertex(250,225);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(250,225);
+   vertex(300,200);
+   vertex(300,250);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(250,225);
+   vertex(300,250);
+   vertex(300,300);
+   vertex(250,275);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(250,275);
+   vertex(300,300);
+   vertex(250,325);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(250,325);
+   vertex(300,300);
+   vertex(300,350);
+   vertex(250,375);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(250,375);
+   vertex(300,350);
+   vertex(300,400);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(250,375);
+   vertex(300,400);
+   vertex(300,450);
+   vertex(250,425);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(300,0);
+   vertex(350,-25);
+   vertex(350,0);
+   vertex(350,25);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(300,0);
+   vertex(350,25);
+   vertex(350,75);
+   vertex(300,50);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(300,50);
+   vertex(350,75);
+   vertex(300,100);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(300,100);
+   vertex(350,75);
+   vertex(350,125);
+   vertex(300,150);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(300,150);
+   vertex(350,125);
+   vertex(350,150);
+   vertex(350,175);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(300,150);
+   vertex(350,175);
+   vertex(350,225);
+   vertex(300,200);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(300,200);
+   vertex(350,225);
+   vertex(300,250);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(300,250);
+   vertex(350,225);
+   vertex(350,275);
+   vertex(300,300);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(300,300);
+   vertex(350,275);
+   vertex(350,300);
+   vertex(350,325);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(300,300);
+   vertex(350,325);
+   vertex(350,375);
+   vertex(300,350);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(300,350);
+   vertex(350,375);
+   vertex(300,400);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(300,400);
+   vertex(350,375);
+   vertex(350,425);
+   vertex(300,450);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(350,-25);
+   vertex(400,0);
+   vertex(350,25);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(350,25);
+   vertex(400,0);
+   vertex(400,50);
+   vertex(350,75);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(350,75);
+   vertex(400,50);
+   vertex(400,100);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(350,75);
+   vertex(400,100);
+   vertex(400,150);
+   vertex(350,125);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(350,125);
+   vertex(400,150);
+   vertex(350,175);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(350,175);
+   vertex(400,150);
+   vertex(400,200);
+   vertex(350,225);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(350,225);
+   vertex(400,200);
+   vertex(400,250);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(350,225);
+   vertex(400,250);
+   vertex(400,300);
+   vertex(350,275);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(350,275);
+   vertex(400,300);
+   vertex(350,325);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(350,325);
+   vertex(400,300);
+   vertex(400,350);
+   vertex(350,375);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(350,375);
+   vertex(400,350);
+   vertex(400,400);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(350,375);
+   vertex(400,400);
+   vertex(400,450);
+   vertex(350,425);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(400,0);
+   vertex(450,-25);
+   vertex(450,0);
+   vertex(450,25);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(400,0);
+   vertex(450,25);
+   vertex(450,75);
+   vertex(400,50);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(400,50);
+   vertex(450,75);
+   vertex(400,100);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(400,100);
+   vertex(450,75);
+   vertex(450,125);
+   vertex(400,150);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(400,150);
+   vertex(450,125);
+   vertex(450,150);
+   vertex(450,175);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(400,150);
+   vertex(450,175);
+   vertex(450,225);
+   vertex(400,200);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(400,200);
+   vertex(450,225);
+   vertex(400,250);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(400,250);
+   vertex(450,225);
+   vertex(450,275);
+   vertex(400,300);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(400,300);
+   vertex(450,275);
+   vertex(450,300);
+   vertex(450,325);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(400,300);
+   vertex(450,325);
+   vertex(450,375);
+   vertex(400,350);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(400,350);
+   vertex(450,375);
+   vertex(400,400);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(400,400);
+   vertex(450,375);
+   vertex(450,425);
+   vertex(400,450);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(450,-25);
+   vertex(500,0);
+   vertex(450,25);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(450,25);
+   vertex(500,0);
+   vertex(500,50);
+   vertex(450,75);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(450,75);
+   vertex(500,50);
+   vertex(500,100);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(450,75);
+   vertex(500,100);
+   vertex(500,150);
+   vertex(450,125);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(450,125);
+   vertex(500,150);
+   vertex(450,175);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(450,175);
+   vertex(500,150);
+   vertex(500,200);
+   vertex(450,225);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(450,225);
+   vertex(500,200);
+   vertex(500,250);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(450,225);
+   vertex(500,250);
+   vertex(500,300);
+   vertex(450,275);
+   endShape(CLOSE);
+
+
+   fill(100);
+   beginShape();
+   vertex(450,275);
+   vertex(500,300);
+   vertex(450,325);
+   endShape(CLOSE);
+
+
+   fill(125);
+   beginShape();
+   vertex(450,325);
+   vertex(500,300);
+   vertex(500,350);
+   vertex(450,375);
+   endShape(CLOSE);
+
+
+   fill(0);
+   beginShape();
+   vertex(450,375);
+   vertex(500,350);
+   vertex(500,400);
+   endShape(CLOSE);
+
+
+   fill(255);
+   beginShape();
+   vertex(450,375);
+   vertex(500,400);
+   vertex(500,450);
+   vertex(450,425);
+   endShape(CLOSE);
+
+
+}
