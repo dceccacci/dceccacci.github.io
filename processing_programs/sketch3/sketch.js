f385f87ec205e12827a5d2fb353d2d1d938afc3f
dceccacci <71663434+dceccacci@users.noreply.github.com>
2020-11-16 18:59:37 -0800

    ughs
    trying to get sketch 3 to work.

--- processing_programs/sketch3/sketch.js
+++ processing_programs/sketch3/sketch.js
@@ -1,22 +1,49 @@
-let song;
-let slider;
-
-function preload() {
-    song = loadSound('twofeet.mp3');
-}
+var song;
+var slider;
+var button;
+var jumpButton;
 
 function setup() {
-    createCanvas(200,200);
-    song = loadSound("twofeet.mp3", loaded);
-    slider = createSlider(0, 1, 0, 0.01);
-    song.play();
+    createCanvas(400,300);
+    song = loadSound("twofeet.mp3");
+    buttonplay = createButton("play");
+    buttonplay.mousePressed(togglePlaying);
+    jumpButton = createButton("jump");
+    jumpButton.mousePressed(jumpSong);
+    sliderRate = createSlider(0, 2, 1, 0.01);
+    sliderVolume = createSlider(0, 1, 0.3, 0.01);
+    background(0);
 }
 
-function loaded() {
-    song.play();
+
+function togglePlaying() {
+    if (!song.isPlaying()) {
+        song.play();
+        song.setVolume(0.3);
+        buttonplay.html("stop");
+        setTimeout(changeBackground, 1290);
+    } else {
+        song.stop();
+        buttonplay.html("play");
+    }
 }
+
+function jumpSong() {
+    let len = song.duration();
+    song.jump(random(len));
+}
+
 
 function draw() {
-    background(0);
-    song.setVolume(slider.value());
+    //background(song.currentTime(), 0, 255);
+    song.setVolume(sliderVolume.value());
+    song.rate(sliderRate.value());
+}
+
+function changeBackground() {
+    if (song.isPlaying()){
+        background(random(255), random(255), random(255));
+        setTimeout(changeBackground, 1640);
+
+    }
 }
