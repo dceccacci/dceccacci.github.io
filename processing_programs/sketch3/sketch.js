2819926be9323f631d302a1467a637d9d383d439
dceccacci <71663434+dceccacci@users.noreply.github.com>
2020-11-28 12:41:31 -0800

    s

--- processing_programs/sketch3/sketch.js
+++ processing_programs/sketch3/sketch.js
@@ -5,9 +5,12 @@ var jumpButton;
 var sliderRate;
 var sliderVolume;
 
+function preload(){
+    song = loadSound("inossi-revive.mp3");
+}
+
 function setup() {
     createCanvas(400,300);
-    song = loadSound("inossi-revive.mp3");
     buttonplay = createButton("play");
     buttonplay.mousePressed(togglePlaying);
     jumpButton = createButton("jump");
@@ -15,10 +18,6 @@ function setup() {
     sliderRate = createSlider(0, 2, 1, 0.01);
     sliderVolume = createSlider(0, 1, 0.3, 0.01);
     background(0);
-    Text("Revive by INOSSI | https://soundcloud.com/inossi",0,0);
-    Text("Music promoted by https://www.free-stock-music.com",0,0);
-    Text("Creative Commons Attribution 3.0 Unported License",0,0);
-    Text("https://creativecommons.org/licenses/by/3.0/deed.en_US",0,0);
 }
 
 
